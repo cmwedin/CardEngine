904c0bc5ed39ccbda770e7d2bf79790fdc44d5f1
cmwedin <cmwedin@gmail.com>
2022-06-23 17:15:00 -0400

    Type scriptable object

--- Runtime/SOClasses/TypeSO.cs
+++ Runtime/SOClasses/TypeSO.cs
@@ -1,9 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace SadSapphicGames.CardEngine
 {
     [CreateAssetMenu(fileName = "CardSO", menuName = "SadSapphicGames/CardEngine/TypeSO", order = 1)]
     public class TypeSO : ScriptableObject {
-        
+        private void OnEnable() {
+            if(typeComponent == null) {
+                Debug.LogWarning($"Type Scriptable Object {this.name} does not have an associated monobehavior component. Create a monobehaviour with the same name as the scriptable object");
+            }
+        }
+        private Type typeComponent  { get => Type.GetType(this.name); }
+        public void AddType(Card card) {
+            card.gameObject.AddComponent(typeComponent); 
+        }
     }
 }
