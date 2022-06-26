6d3dfae1e11b1594abc104b535e40b52536d1644
cmwedin <cmwedin@gmail.com>
2022-06-26 12:33:40 -0400

    DecklistSO
    scriptable object for the creation of different decklists

---
+++ Runtime/SOClasses/DecklistSO.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SadSapphicGames.CardEngine {
+    [System.Serializable]
+    public struct DecklistEntry {
+        public CardSO card;
+        public int number;
+    }
+    
+    [CreateAssetMenu(fileName = "DecklistSO", menuName = "SadSapphicGames/CardEngine/DecklistSO", order = 2)]
+    public class DecklistSO : ScriptableObject {
+        public List<DecklistEntry> deckList;
+    }
+}
