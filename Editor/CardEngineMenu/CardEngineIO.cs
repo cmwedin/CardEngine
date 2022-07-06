1962fd2263e414a95a844aa7758186018efb6607
cmwedin <cmwedin@gmail.com>
2022-07-06 10:06:12 -0400

    CardEngineIO.GetPackagePath
    determines if the package is located in the assets folder or the package folder, currently does not handle non-standard locations

---
+++ Editor/CardEngineMenu/CardEngineIO.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace SadSapphicGames.CardEngineEditor {
+    public static class CardEngineIO {
+        private static Directories directories;
+        public static string GetPackagePath() {
+            // ? standard package location
+            string packagePath = "Packages/com.sadsapphicgames.cardengine";
+            if(Directory.Exists(packagePath)) {
+                return packagePath;
+            } 
+            // ? location for package development
+            packagePath = "Assets/Packages/CardEngine";
+            if (Directory.Exists(packagePath)) {
+                return packagePath;
+            }
+            // ? failed to find
+            return null;
+        }
+        public static void UpdateDirectories() {
+            directories = SettingsEditor.ReadSettings().Directories;
+        }
+    }
+}
