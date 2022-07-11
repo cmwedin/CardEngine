b556f8f9e56dc033957aa345d2518d01a6741e29
cmwedin <cmwedin@gmail.com>
2022-07-11 12:28:58 -0400

    CardType subdata integration
    adding a type to a card now creates an appropriate typed scriptable object for that Card Types subdata

--- Runtime/SOClasses/CardSO.cs
+++ Runtime/SOClasses/CardSO.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 
@@ -14,11 +16,32 @@ namespace SadSapphicGames.CardEngine
         public Sprite CardSprite {get => _cardSprite;}
         [SerializeField] private string _cardText;
         public string CardText {get => _cardText; set => _cardText = value;}
-        [SerializeField] private List<TypeSO> _cardTypes;
+        [SerializeField] private List<TypeSO> _cardTypes = new List<TypeSO>();
         public List<TypeSO> CardTypes {get => _cardTypes;}
+        
+        private Dictionary<TypeSO,TypeDataSO> typesSubData = new Dictionary<TypeSO, TypeDataSO>();
 
         public void AddType(DatabaseEntry<TypeSO> typeDatabaseEntry) {
-            CardTypes.Add(typeDatabaseEntry.entrykey);
+            TypeSO typeSO = typeDatabaseEntry.entrykey;
+            Type typeDataSOType = typeSO.TypeDataReference.GetType(); 
+                //? this name is pretty confusing: 
+                //? this is the type of the scriptable object representing the data associated with the card-type we are adding
+            CardTypes.Add(typeSO);
+
+            TypeDataSO typeData = (TypeDataSO)ScriptableObject.CreateInstance(typeDataSOType);
+            typeData.name = $"{CardName}{typeSO.Name}Data";
+            AssetDatabase.CreateAsset(typeData,$"{CardDatabaseSO.instance.GetEntryByKey(this).entryDirectory}/{typeData.name}.asset");
+            typesSubData.Add(typeSO,typeData);
+            
+            AssetDatabase.SaveAssets();
+        }
+        public TypeDataSO GetTypeSubdata(TypeSO type) {
+            if (!CardTypes.Contains(type)) {
+                Debug.LogWarning($"{CardName} does not have type {type.Name}");
+                return null;
+            } else {
+                return typesSubData[type];
+            }
         }
     }
 }
