7d7f874d7c2635825558cbef3fbd2b67c7ba0dd9
cmwedin <cmwedin@gmail.com>
2022-07-14 16:08:17 -0400

    EffectSO editor
    initial attempt at adding subeffects to an effectSO, needs work

--- Editor/CustomSOInspector/EffectSOEditor.cs
+++ Editor/CustomSOInspector/EffectSOEditor.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEditor;
 using SadSapphicGames.CardEngine;
+using System;
 
 namespace SadSapphicGames.CardEngineEditor
 {
@@ -9,11 +10,24 @@ namespace SadSapphicGames.CardEngineEditor
 public class EffectSOEditor : Editor {
     public override void OnInspectorGUI() {
         base.OnInspectorGUI();
+        int controlID = EditorGUIUtility.GetControlID(FocusType.Passive);
         if(GUILayout.Button("Add Subeffect")) {
             Debug.Log("opening subeffect window");
-            PopupWindow.Show(new Rect(), new SelectEffectPopup((EffectSO)target));
+            // PopupWindow.Show(new Rect(), new SelectEffectPopup((EffectSO)target));
+            EditorGUIUtility.ShowObjectPicker<UnitEffectSO>(null,false,"",controlID);
         }
-        
+        if(Event.current.commandName == "ObjectSelectorUpdated" && EditorGUIUtility.GetObjectPickerControlID() ==  controlID) {
+            AddUnitEffect((UnitEffectSO)EditorGUIUtility.GetObjectPickerObject());
+        } else if (Event.current.commandName == "ObjectSelectorClosed" && EditorGUIUtility.GetObjectPickerControlID() ==  controlID) {
+            Debug.Log("Object selector closed");
+        }
+    }
+    private void AddUnitEffect(UnitEffectSO unitEffect)  {
+            Type subEffectType = unitEffect.GetType();
+            Debug.Log($"adding subeffect {unitEffect.name}");
+            var subEffectObj = ScriptableObject.CreateInstance(subEffectType);
+            AssetDatabase.AddObjectToAsset(subEffectObj,AssetDatabase.GetAssetPath(target));
+            AssetDatabase.SaveAssets();
     }
 }
 }
