fdfbd9ee95bbc770c011f6db8afee86c283cf666
cmwedin <cmwedin@gmail.com>
2022-07-15 14:45:05 -0400

    improved Effect editor
    removed bug where subeffects are added multiple times, adds subeffect to the list of them in the composite

--- Editor/CustomSOInspector/CompositeEffectSOEditor.cs
+++ Editor/CustomSOInspector/CompositeEffectSOEditor.cs
@@ -8,15 +8,19 @@ namespace SadSapphicGames.CardEngineEditor
 
 [CustomEditor(typeof(CompositeEffectSO))]
 public class CompositeEffectSOEditor : Editor {
+    bool selectionMade = true;
+    
     public override void OnInspectorGUI() {
         base.OnInspectorGUI();
         int controlID = EditorGUIUtility.GetControlID(FocusType.Passive);
         if(GUILayout.Button("Add Subeffect")) {
+            selectionMade = false;
             Debug.Log("opening subeffect window");
             // PopupWindow.Show(new Rect(), new SelectEffectPopup((EffectSO)target));
             EditorGUIUtility.ShowObjectPicker<UnitEffectSO>(null,false,"",controlID);
         }
-        if(Event.current.commandName == "ObjectSelectorUpdated" && EditorGUIUtility.GetObjectPickerControlID() ==  controlID) {
+        if(Event.current.commandName == "ObjectSelectorUpdated" && EditorGUIUtility.GetObjectPickerControlID() ==  controlID && selectionMade == false) {
+            selectionMade = true;
             AddUnitEffect((UnitEffectSO)EditorGUIUtility.GetObjectPickerObject());
         } else if (Event.current.commandName == "ObjectSelectorClosed" && EditorGUIUtility.GetObjectPickerControlID() ==  controlID) {
             Debug.Log("Object selector closed");
@@ -24,10 +28,14 @@ public class CompositeEffectSOEditor : Editor {
     }
     private void AddUnitEffect(UnitEffectSO unitEffect)  {
             Type subEffectType = unitEffect.GetType();
+            CompositeEffectSO targetObj = (CompositeEffectSO)target;
             Debug.Log($"adding subeffect {unitEffect.name}");
-            var subEffectObj = ScriptableObject.CreateInstance(subEffectType);
-            AssetDatabase.AddObjectToAsset(subEffectObj,AssetDatabase.GetAssetPath(target));
+            EffectSO subEffectObj = (EffectSO)ScriptableObject.CreateInstance(subEffectType);
+            subEffectObj.name = $"{targetObj.name}Subeffect{targetObj.ChildrenCount + 1}";
+            AssetDatabase.AddObjectToAsset(subEffectObj,AssetDatabase.GetAssetPath(targetObj));
+            targetObj.AddChild(subEffectObj);
             AssetDatabase.SaveAssets();
+
     }
 }
 }
--- Runtime/CardEffects/CompositeEffectSO.cs
+++ Runtime/CardEffects/CompositeEffectSO.cs
@@ -6,10 +6,14 @@ namespace SadSapphicGames.CardEngine
     [CreateAssetMenu(fileName = "CompositeEffectSO", menuName = "CardEngineDevelopment/CompositeEffectSO", order = 0)]
     public class CompositeEffectSO : EffectSO {
         [SerializeReference] List<EffectSO> subEffects = new List<EffectSO>();
+        public int ChildrenCount { get => subEffects.Count;}
         public override void ResolveEffect() {
             foreach (var effect in subEffects) {
                 effect.ResolveEffect();                
             }
+        }
+        public void AddChild(EffectSO childEffect) {
+            subEffects.Add(childEffect);
         }
     }
 }
