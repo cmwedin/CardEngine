3e82b1f898b1ea8a224bb29f12c13853445173c3
cmwedin <cmwedin@gmail.com>
2022-07-16 13:05:29 -0400

    UnitEffectTemplate

---
+++ Editor/Templates/UnitEffectTemplate.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SadSapphicGames.CardEngine;
+public class UnitEffectTemplate : UnitEffectSO {
+    public override void ResolveEffect()
+    {
+        throw new System.NotImplementedException();
+    }
+}
