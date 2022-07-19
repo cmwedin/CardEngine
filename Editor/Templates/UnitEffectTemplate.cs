6a531434f66174d5159018a58622adfc6792c707
cmwedin <cmwedin@gmail.com>
2022-07-19 11:39:21 -0400

    added palceholder createassetmenu for unit effects
    this is for use until the issues with creating the scriptable object automatically is resolved

--- Editor/Templates/UnitEffectTemplate.cs
+++ Editor/Templates/UnitEffectTemplate.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SadSapphicGames.CardEngine;
+
+[CreateAssetMenu(menuName = "UnitEffectPlaceholder/UnitEffectTemplate")]
 public class UnitEffectTemplate : UnitEffectSO {
     public override void ResolveEffect()
     {
