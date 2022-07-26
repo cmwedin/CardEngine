1e227582cece61f952c8449f2b538e21142508cd
cmwedin <cmwedin@gmail.com>
2022-07-26 11:32:05 -0400

    CreateCardWindow object pattern
    implements the object design pattern for the logic of the CreateCardWindow

--- Editor/CardEngineCreate/CreateCardSOWindow.cs
+++ Editor/CardEngineCreate/CreateCardSOWindow.cs
@@ -7,16 +7,11 @@ using UnityEditor;
 using SadSapphicGames.CardEngine;
 
 namespace SadSapphicGames.CardEngineEditor {
-    public class CreateCardWindow : EditorWindow {
-        string cardName;
-        string cardText;
+    public class CreateCardObject {
         string cardsDirectory;
         CardDatabaseSO cardDatabase;
-        bool closeWindow;
-        public CreateCardWindow() : base() {
-            
-        }
-        private void OnEnable() {
+        public bool closeWindow = false;
+        public CreateCardObject() {
             var settings = SettingsEditor.ReadSettings(); 
             cardDatabase = CardDatabaseSO.Instance;
 
@@ -31,28 +26,16 @@ namespace SadSapphicGames.CardEngineEditor {
                 }
             }
         }
-        [MenuItem("CardEngine/Create/Card")]
-        static void Init() {
-            EditorWindow window = EditorWindow.CreateInstance<CreateCardWindow>();
-            window.Show();
-        }
-        private void OnGUI() {
-            if(closeWindow) this.Close();
-            GUILayout.Label("Create a card type", EditorStyles.boldLabel);
-            GUILayout.BeginVertical();
-                cardName = EditorGUILayout.TextField("Enter card name",cardName);
-                GUILayout.Label("Card text:");
-                cardText = EditorGUILayout.TextArea(cardText);
-            GUILayout.EndVertical();
-            GUILayout.BeginHorizontal();
-                if(GUILayout.Button("Create Card",EditorStyles.miniButtonLeft)) {
-                    if(cardName == "") {
+        public void CreateCard(string cardName, string cardText) {
+            if(cardName == "") {
                         Debug.LogWarning("Card name required");
-                        this.Close();
+                        closeWindow = true;
+                        return;
                     }
                     if(Directory.Exists(cardsDirectory + "/" + cardName)) {
-                        this.Close();
-                        throw new Exception($"Folder for card {cardName} already exists");
+                        closeWindow = true;
+                        Debug.LogWarning($"Folder for card {cardName} already exists");
+                        return;
                     }
                     AssetDatabase.CreateFolder(cardsDirectory, cardName);
                     string cardPath = cardsDirectory + "/" + cardName;
@@ -71,7 +54,37 @@ namespace SadSapphicGames.CardEngineEditor {
                     AssetDatabase.SaveAssets();
                     cardDatabase.AddEntry(cardSO, cardPath);
 
-                    this.Close();
+                    EditorUtility.SetDirty(cardSO);
+                    closeWindow = true;
+        }
+    }
+    public class CreateCardWindow : EditorWindow {
+        string cardName;
+        string cardText;
+        public CreateCardObject windowObject;
+        
+        public CreateCardWindow() : base() {
+        }
+        private void OnEnable() {
+            
+        }
+        [MenuItem("CardEngine/Create/Card")]
+        static void Init() {
+            CreateCardWindow window = EditorWindow.CreateInstance<CreateCardWindow>();
+            window.windowObject = new CreateCardObject();
+            window.Show();
+        }
+        private void OnGUI() {
+            if(windowObject.closeWindow) this.Close();
+            GUILayout.Label("Create a card type", EditorStyles.boldLabel);
+            GUILayout.BeginVertical();
+                cardName = EditorGUILayout.TextField("Enter card name",cardName);
+                GUILayout.Label("Card text:");
+                cardText = EditorGUILayout.TextArea(cardText);
+            GUILayout.EndVertical();
+            GUILayout.BeginHorizontal();
+                if(GUILayout.Button("Create Card",EditorStyles.miniButtonLeft)) {
+                    windowObject.CreateCard(cardName,cardText);
                 }
                 if(GUILayout.Button("Cancel",EditorStyles.miniButtonRight)) {
                     this.Close();
