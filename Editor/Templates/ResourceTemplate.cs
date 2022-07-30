a860c530bce0a05bb67e9d9f98758b11e5a24d13
cmwedin <cmwedin@gmail.com>
2022-07-30 14:15:27 -0400

    ResourceTemplate file

---
+++ Editor/Templates/ResourceTemplate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SadSapphicGames.CardEngine;
+
+public class ResourceTemplate : ResourceSO
+{
+    public override void PayResource(AbstractActor actorPaying)
+    {
+        throw new System.NotImplementedException();
+    }
+}
