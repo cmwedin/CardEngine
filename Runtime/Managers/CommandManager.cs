b7106c8aaa9a1875f4aa7cdb265a0a5bda7dd6dc
cmwedin <cmwedin@gmail.com>
2022-08-04 11:36:00 -0400

    CommandManager.UndoUntilCommand
    adds method to command manager to undo commands until a specific command is reached, an optional paramter (default false) determines if the threshold command is undone as well. Throws and exception if command is not in the list of executed commands

--- Runtime/Managers/CommandManager.cs
+++ Runtime/Managers/CommandManager.cs
@@ -30,6 +30,26 @@ namespace SadSapphicGames.CardEngine {
                 freezeCommandExecution = false;
             }
         }
+        public void UndoUntilCommand(Command stopAtCommand, bool inclusive = false) {
+            if(!executedCommands.Contains(stopAtCommand)) {
+                throw new System.ArgumentException("Argument not contained in previously executed commands");
+            }
+            freezeCommandExecution = true;
+            while(executedCommands.TryPop(out Command previousCommand)) {
+                if(previousCommand != stopAtCommand) {
+                    previousCommand.Undo();
+                } else { //? we have reached the command to stop at
+                    if(!inclusive) { //? if we don't what to undo the argument as well put it back on the executed stack
+                        executedCommands.Push(previousCommand);
+                    } else { //? otherwise undo it
+                        previousCommand.Undo();
+                    }
+                    //? break out of the loop
+                    break;
+                }
+            }
+            freezeCommandExecution = false;
+        }
         private void Awake() {
             if(instance != null && instance != this) {
                 Destroy(this);
