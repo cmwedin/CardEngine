b0cc4a7308807f121e7ea9cebb94183d298782e1
cmwedin <cmwedin@gmail.com>
2022-08-04 12:06:39 -0400

    CompositeCommand class
    implements an abstract class using the composite pattern for commands that have multiple subcommands that should only be executed or undone as a group

---
+++ Runtime/Commands/CompositeCommand.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace SadSapphicGames.CardEngine
+{
+    public abstract class CompositeCommand : Command {
+        private List<Command> subcommands = new List<Command>();
+
+        public override bool Execute() {
+            List<Command> executedSubcommands = new();
+            foreach (var command in subcommands) {
+                if (command.Execute()) {
+                    executedSubcommands.Add(command);
+                } else {
+                    foreach (var executedCommand in executedSubcommands) {
+                        executedCommand.Undo();
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
+        public override void Undo() {
+            foreach (var command in subcommands) {
+                command.Undo();
+            }
+        }
+    }
+}
