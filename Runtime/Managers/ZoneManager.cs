923f0a8c76551cc2ddd44e7c0a8bf37fbe3d2a64
cmwedin <cmwedin@gmail.com>
2022-08-06 14:28:20 -0400

    ZoneManager singleton
    new singleton class to allow easy access to references to different zones from anywhere

---
+++ Runtime/Managers/ZoneManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+    namespace SadSapphicGames.CardEngine {
+    public class ZoneManager : MonoBehaviour {
+        public ZoneManager instance;
+        public StagingZone stagingZone;
+
+        private void Awake() {
+            if(instance != null && instance != this) {
+                Destroy(this);
+            } else {
+                instance = this;
+            }
+        }
+    }
+}
