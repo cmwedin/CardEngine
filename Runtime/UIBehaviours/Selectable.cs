e3b4622c7554cd55c170b0ec4ab8126d6716b0ba
cmwedin <cmwedin@gmail.com>
2022-08-17 16:04:38 -0400

    Selectable bugfix
    now properly detects clicking on a selectable object

--- Runtime/UIBehaviours/Selectable.cs
+++ Runtime/UIBehaviours/Selectable.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 
 namespace SadSapphicGames.CardEngine
 {
-    public class Selectable : MonoBehaviour, ISelectHandler {
+    public class Selectable : MonoBehaviour, ISelectHandler, IPointerDownHandler {
         public event Action<GameObject> SelectionMade;
-        public void OnSelect(BaseEventData eventData)
-        {
+
+        public void OnPointerDown(PointerEventData eventData) {
+            EventSystem.current.SetSelectedGameObject(gameObject, eventData);
+        }
+
+        public void OnSelect(BaseEventData eventData) {
             SelectionMade?.Invoke(this.gameObject);
             //? this should always be a listener but just in case
         }
