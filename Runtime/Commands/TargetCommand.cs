b9f49ba4330f0b268404dac067e8e33eda2a8083
cmwedin <cmwedin@gmail.com>
2022-08-17 13:45:58 -0400

    TargetCommand Selectable integration
    with this commit targeting should be functional, need to test

--- Runtime/Commands/TargetCommand.cs
+++ Runtime/Commands/TargetCommand.cs
@@ -15,19 +15,29 @@ namespace SadSapphicGames.CardEngine
             this.effectToTarget = effectToTarget;
             validTargets = (GameObject[])GameObject.FindObjectsOfType(effectToTarget.TargetType);
         }
+        public TargetCommand(ITargeted effectToTarget, IEnumerable<GameObject> validTargets){
+            this.effectToTarget = effectToTarget;
+            this.validTargets = validTargets;
+        }
 
         public override void Execute() {
+            Debug.Log($"Please select a target for {((EffectSO)effectToTarget).name}"); //? this will be displayed on screen eventually
             foreach (var gameObject in validTargets) {
+                Selectable selectable = gameObject.AddComponent<Selectable>();
+                selectable.SelectionMade += OnSelectionMade;
             }
-            while(!selectionMade) { //? wait until OnSelectionMade gets invoked
-            }
+            while (!selectionMade) { } //? wait until OnSelectionMade gets invoked
+            if (selectedTarget == null) { throw new System.Exception($"Failure to select target for {((EffectSO)effectToTarget).name}"); }
+            effectToTarget.Target = selectedTarget;
         }
 
         private void OnSelectionMade(GameObject selection) {
             foreach(var gameObject in validTargets) {
+                UnityEngine.Object.Destroy(gameObject.GetComponent<Selectable>());
             }
             selectedTarget = selection;
             selectionMade = true;
+            Debug.Log($"{((EffectSO)effectToTarget).name} targeted to {selection.GetComponent<Card>().name}");
         }
     }
 }
