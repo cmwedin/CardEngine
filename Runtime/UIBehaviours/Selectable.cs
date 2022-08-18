eb296ed64d642cb0f93226bb5da7a7674b1aa66e
cmwedin <cmwedin@gmail.com>
2022-08-18 12:28:15 -0400

    Simple selectable highlighting
    adds a simple outline to objects with the selectable component, removed when the selectable component is destroyed

--- Runtime/UIBehaviours/Selectable.cs
+++ Runtime/UIBehaviours/Selectable.cs
@@ -8,6 +8,7 @@ namespace SadSapphicGames.CardEngine
 {
     public class Selectable : MonoBehaviour, ISelectHandler, IPointerDownHandler {
         public event Action<GameObject> SelectionMade;
+        Outline highlight;
 
         public void OnPointerDown(PointerEventData eventData) {
             EventSystem.current.SetSelectedGameObject(gameObject, eventData);
@@ -16,6 +17,14 @@ namespace SadSapphicGames.CardEngine
         public void OnSelect(BaseEventData eventData) {
             SelectionMade?.Invoke(this.gameObject);
             //? this should always be a listener but just in case
+        }
+        private void OnEnable() {
+            highlight = gameObject.AddComponent<Outline>();
+            highlight.effectColor = Color.cyan;
+            highlight.effectDistance = new Vector2(2.5f, 2.5f);
+        }
+        private void OnDestroy() {
+            Destroy(highlight);
         }
     }
 }
