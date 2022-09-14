dffb2242429823114ceb2429918524f974e8be01
cmwedin <cmwedin@gmail.com>
2022-09-14 13:22:27 -0400

    Adds xml comments to config editor files

--- Editor/CardEngineConfig/SettingsEditor.cs
+++ Editor/CardEngineConfig/SettingsEditor.cs
@@ -6,10 +6,16 @@ using System.IO;
 using SadSapphicGames.CardEngine;
 
 namespace SadSapphicGames.CardEngineEditor {
-    [System.Serializable] //? we use a class not a struct for the root object so that it is nullable
-    public class Settings {
+    //? we use a class not a struct for the root object so that it is nullable
+    /// <summary>
+    /// The class used to represent the settings json
+    /// </summary>
+    [System.Serializable] public class Settings {
         public Directories Directories;
     }
+    /// <summary>
+    /// The struct used to store the values of the directories entry of the setting json
+    /// </summary>
     [System.Serializable]
     public struct Directories {
         public string CardTypes;
@@ -17,19 +23,25 @@ namespace SadSapphicGames.CardEngineEditor {
         public string Effects;
         public string Resources;
     }
-    public class SettingsEditor : EditorWindow {
-
+    /// <summary>
+    /// the static class used to modify the entries of the settings json
+    /// </summary>
+    public static class SettingsEditor {
+        /// <summary>
+        /// The path for the settings json
+        /// </summary>
         static string settingsPath = $"Assets/CardEngine/config/settings.json";
-        // [MenuItem("CardEngine/Settings")]
-        // private static void ShowWindow() {
-        //     var window = GetWindow<SettingsEditor>();
-        //     window.titleContent = new GUIContent("Settings");
-        //     window.Show();
-        // }
-        [MenuItem("CardEngine/Settings/Set CardType directory")]
-        private static void SetCardTypeDirectory() {
-            var settings = ReadSettings();
-            if(settings == null) return; 
+
+        /// <summary>
+        /// The menu item to set the value of the CardType directory in the settings json
+        /// </summary>
+        [MenuItem("Tools/CardEngine/Directories/Set CardType directory")] private static void SetCardTypeDirectory() {
+            Settings settings;
+            try {
+                settings = ReadSettings();
+            } catch (SettingsNotFoundException) {
+                return;
+            }
             
             string path = ConvertAbsoluteToRelativePath(EditorUtility.OpenFolderPanel("Select Directory","",""));
             if(path == "") return;
@@ -37,10 +49,16 @@ namespace SadSapphicGames.CardEngineEditor {
             settings.Directories.CardTypes = path;
             WriteSettings(settings);
         }
-        [MenuItem("CardEngine/Settings/Set Effect directory")]
-        private static void SetEffectDirectory() {
-            var settings = ReadSettings();
-            if(settings == null) return; 
+        /// <summary>
+        /// The menu item to set the value of the Effect directory in the settings json
+        /// </summary>
+        [MenuItem("Tools/CardEngine/Directories/Set Effect directory")] private static void SetEffectDirectory() {
+            Settings settings;
+            try {
+                settings = ReadSettings();
+            } catch (SettingsNotFoundException) {
+                return;
+            }
 
             string path = ConvertAbsoluteToRelativePath(EditorUtility.OpenFolderPanel("Select Directory","",""));
             if(path == "") return;
@@ -48,10 +66,16 @@ namespace SadSapphicGames.CardEngineEditor {
             settings.Directories.Effects = path;
             WriteSettings(settings);
         }
-        [MenuItem("CardEngine/Settings/Set CardScriptableObject directory")]
-        private static void SetCardScriptableObjectDirectory() {
-            var settings = ReadSettings();
-            if(settings == null) return;
+        /// <summary>
+        /// The menu item to set the value of the CardSO directory in the settings json
+        /// </summary>
+        [MenuItem("Tools/CardEngine/Directories/Set CardScriptableObject directory")] private static void SetCardScriptableObjectDirectory() {
+            Settings settings;
+            try {
+                settings = ReadSettings();
+            } catch (SettingsNotFoundException) {
+                return;
+            }
 
             string path = ConvertAbsoluteToRelativePath(EditorUtility.OpenFolderPanel("Select Directory","",""));
             if(path == "") return;
@@ -59,10 +83,16 @@ namespace SadSapphicGames.CardEngineEditor {
             settings.Directories.CardScriptableObjects = path;
             WriteSettings(settings);
         }
-        [MenuItem("CardEngine/Settings/Set Resource directory")]
-        private static void SetResourceDirectory() {
-            var settings = ReadSettings();
-            if(settings == null) return;
+        /// <summary>
+        /// The menu item to set the value of the Resource directory in the settings json
+        /// </summary>
+        [MenuItem("CardEngine/Settings/Set Resource directory")] private static void SetResourceDirectory() {
+            Settings settings;
+            try {
+                settings = ReadSettings();
+            } catch (SettingsNotFoundException) {
+                return;
+            }
 
             string path = ConvertAbsoluteToRelativePath(EditorUtility.OpenFolderPanel("Select Directory","",""));
             if(path == "") return;
@@ -70,44 +100,55 @@ namespace SadSapphicGames.CardEngineEditor {
             settings.Directories.Resources = path;
             WriteSettings(settings);
         }
-        private void OnGUI() {
-            // if(GUILayout.Button("Select CardType Directory")) {
-            //     string path = EditorUtility.OpenFolderPanel("Select Directory","","");
-            //     var settings = ReadSettings();
-            //     settings.Directories.CardTypes = path;
-            //     WriteSettings(settings);
-            // }
-            // if(GUILayout.Button("Select CardScriptableObject Directory")) {
-            //     string path = EditorUtility.OpenFolderPanel("Select Directory","","");
-            //     var settings = ReadSettings();
-            //     settings.Directories.CardScriptableObjects = path;
-            //     WriteSettings(settings);
-            // }
-        }
+
+        /// <summary>
+        /// Converts the contents of the settings json into a Settings object
+        /// </summary>
+        /// <returns> The settings object created from the current values of the settings json</returns>
         public static Settings ReadSettings() {
             if(!CardEngineInit.EssentialsImported) {
                 Debug.LogWarning("Settings file not found, please initialize");
                 CardEngineInitWindow.showInitWindow();
-                return null;
+                throw new SettingsNotFoundException(); 
             }
             using StreamReader reader = new StreamReader(settingsPath);
             string json = reader.ReadToEnd();
             reader.Close();
-            // Debug.Log($"settings json: {json}");
             Settings settings = JsonUtility.FromJson<Settings>(json);
-            // Debug.Log($"settings struct values, CardType:{settings.Directories.CardTypes},CardScriptableObjects:{settings.Directories.CardScriptableObjects}");
             return settings;
         }
+        /// <summary>
+        /// Sets the values of the settings json to the 
+        /// </summary>
+        /// <param name="settings"></param>
         private static void WriteSettings(Settings settings) {
             string json = JsonUtility.ToJson(settings);
             using StreamWriter writer = new StreamWriter(settingsPath);
             writer.WriteLine(json);
             writer.Close();
         }
+        /// <summary>
+        /// Converts the absolute path of an asset on the hard drive to the relative path of an asset in the project
+        /// </summary>
+        /// <param name="absolutePath">The path of the hard drive</param>
+        /// <returns>the path of an asset in the project</returns>
         public static string ConvertAbsoluteToRelativePath (string absolutePath) {
             if(absolutePath.StartsWith(Application.dataPath)) {
                 return "Assets" + absolutePath.Substring(Application.dataPath.Length);
             } else return null;
         }
+    }
+    /// <summary>
+    /// An exception thrown if the settings file could not be found
+    /// </summary>
+    [System.Serializable]
+    public class SettingsNotFoundException : System.Exception
+    {
+        public SettingsNotFoundException() : base("Failed to find settings.json file") { }
+        public SettingsNotFoundException(string message) : base(message) { }
+        public SettingsNotFoundException(string message, System.Exception inner) : base(message, inner) { }
+        protected SettingsNotFoundException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
