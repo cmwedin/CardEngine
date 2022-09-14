efe12edc3df6bdd4d0f8aff2f0ce6ba8446f4cc0
cmwedin <cmwedin@gmail.com>
2022-09-14 16:12:51 -0400

    database classes xml comments

--- Editor/Databases/CardDatabaseSO.cs
+++ Editor/Databases/CardDatabaseSO.cs
@@ -8,8 +8,17 @@ using SadSapphicGames.CardEngine;
 
 namespace SadSapphicGames.CardEngineEditor
 {
+    /// <summary>
+    /// The database for CardSO's created by the package
+    /// </summary>
     public class CardDatabaseSO : DatabaseSO<CardSO> {
+        /// <summary>
+        /// The Singleton instance of the database
+        /// </summary>
         private static CardDatabaseSO instance;
+        /// <summary>
+        /// Property for retrieving the instance of the database. Opens the InitWindow if essentials haven't been imported
+        /// </summary>
         public static CardDatabaseSO Instance {get {
             if(!CardEngineInit.EssentialsImported) {
                 CardEngineInitWindow.showInitWindow();
--- Editor/Databases/ResourceDatabaseSO.cs
+++ Editor/Databases/ResourceDatabaseSO.cs
@@ -5,9 +5,17 @@ using SadSapphicGames.CardEngine;
 
 namespace SadSapphicGames.CardEngineEditor
 {
-    [CreateAssetMenu]
+    /// <summary>
+    /// The database for ResourceSO's created by the package
+    /// </summary>
     public class ResourceDatabaseSO : DatabaseSO<ResourceSO> {
+        /// <summary>
+        /// The Singleton instance of the database
+        /// </summary>
         private static ResourceDatabaseSO instance;
+        /// <summary>
+        /// Property for retrieving the instance of the database. Opens the InitWindow if essentials haven't been imported
+        /// </summary>
         public static ResourceDatabaseSO Instance {get {
             if(!CardEngineInit.EssentialsImported) {
                 CardEngineInitWindow.showInitWindow();
