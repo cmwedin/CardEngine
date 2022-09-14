d59ed5be95d754f4a36e4e756a58b5d84eb6c34e
cmwedin <cmwedin@gmail.com>
2022-09-14 17:30:41 -0400

    popup window xml comments

--- Editor/PopupWindows/AddEffectPopup.cs
+++ Editor/PopupWindows/AddEffectPopup.cs
@@ -5,23 +5,47 @@ using System.Collections.Generic;
 using System;
 
 namespace SadSapphicGames.CardEngineEditor {
+    /// <summary>
+    /// The object that adds an effect to a CompositeEffectSO
+    /// </summary>
     public class AddEffectObject {
+        /// <summary>
+        /// Communicates to the wrapping popup the window should be closed
+        /// </summary>
         public bool CloseWindow { get; private set;}
+        /// <summary>
+        /// The list of possible effect names
+        /// </summary>
         public List<string> effectNames;
-
+        /// <summary>
+        /// The CompositeEffect to add a new child effect to
+        /// </summary>
         private CompositeEffectSO targetEffect;
+        /// <summary>
+        /// The database of UnitEffectSO's
+        /// </summary>
         private EffectDatabaseSO effectDatabase;    
-
-
+        /// <summary>
+        /// Constructs an AddEffectObject targeting a given CompositeEffectSO
+        /// </summary
+        /// <exception cref="ArgumentNullException">Thrown if the targeted CompositeEffectSO is null</exception>
+        
         public AddEffectObject (CompositeEffectSO _targetEffect) {
+            if(_targetEffect == null) {
+                throw new ArgumentNullException();
+            }
             targetEffect = _targetEffect;
             effectDatabase = EffectDatabaseSO.Instance;
             effectNames = effectDatabase.GetAllObjectNames();
         }
-
+        /// <summary>
+        /// Adds the selected effects to the composite effect
+        /// </summary>
+        /// <param name="effectsToAdd">a boolean array for what effects to add</param>
+        /// <exception cref="ArgumentException">thrown if the length of the argument doesn't </exception>
         public void AddEffects(bool[] effectsToAdd) {
             if(effectsToAdd.Length != effectNames.Count) {
-                throw new Exception("invalid argument length");
+                throw new ArgumentException("invalid argument length");
             }
             for (int i = 0; i < effectsToAdd.Length; i++) {
                 if(effectsToAdd[i]) {
@@ -34,17 +58,37 @@ namespace SadSapphicGames.CardEngineEditor {
             CloseWindow = true;
         }
     }
+    /// <summary>
+    /// The popup window through which the user interacts with the AddEffectObject
+    /// </summary>
     public class AddEffectPopup : PopupWindowContent {
+        /// <summary>
+        /// The AddEffectObject which the window wraps
+        /// </summary>
         private AddEffectObject popupObject;
+        /// <summary>
+        /// The target CompositeEffectSO to add the child effect to
+        /// </summary>
         private CompositeEffectSO targetEffect;
+        /// <summary>
+        /// Gets the effect names from the wrapped AddEffectObject
+        /// </summary>
         private List<string> EffectNames {get => popupObject.effectNames;}
+        /// <summary>
+        /// A boolean list of effects that have been selected to be added
+        /// </summary>
         private bool[] effectsToAdd;
+        /// <summary>
+        /// Construct and AddEffectPopup that will target its wrapped object to a given CompositeEffectSO
+        /// </summary>
         public AddEffectPopup(CompositeEffectSO target) : base() {
             targetEffect = target;
             popupObject = new AddEffectObject(targetEffect);
             effectsToAdd = new bool[EffectNames.Count];
         }
-    
+        /// <summary>
+        /// What will be displayed on the popup window
+        /// </summary>
         public override void OnGUI(Rect rect)
         {
             if(popupObject.CloseWindow) editorWindow.Close();
--- Editor/PopupWindows/AddTypePopup.cs
+++ Editor/PopupWindows/AddTypePopup.cs
@@ -5,12 +5,30 @@ using System.Collections.Generic;
 using System;
 
 namespace SadSapphicGames.CardEngineEditor {
+    /// <summary>
+    /// The Object that adds a Type to a CardSO
+    /// </summary>
     public class AddTypeObject {
+        /// <summary>
+        /// The CardSO to add the type to
+        /// </summary>
         CardSO targetCardSO;
+        /// <summary>
+        /// Communicates to the popup window that wraps this object it should be closed
+        /// </summary>
         public bool CloseWindow { get; private set;}
+        /// <summary>
+        /// The database of typeSO's that may be added
+        /// </summary>
         private TypeDatabaseSO typeDatabase;
+        /// <summary>
+        /// The list of possible typeSO names
+        /// </summary>
         public List<string> typeNames;
-
+        /// <summary>
+        /// Constructs a AddTypeObject targeting a given CardSO
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if the targeted CardSO is null</exception>
         public AddTypeObject(CardSO _targetCardSO) {
             if(_targetCardSO == null) {
                 throw new ArgumentNullException();
@@ -19,9 +37,14 @@ namespace SadSapphicGames.CardEngineEditor {
             typeDatabase = TypeDatabaseSO.Instance;
             typeNames = typeDatabase.GetAllObjectNames();
         }
+        /// <summary>
+        /// Adds the selected types to the CardSO
+        /// </summary>
+        /// <param name="typesToAdd">a boolean array of the selected types</param>
+        /// <exception cref="ArgumentException">thrown if the length of the argument does not equal the length of type names</exception>
         public void AddTypes(bool[] typesToAdd) {
             if(typesToAdd.Length != typeNames.Count) {
-                throw new Exception("invalid argument length");
+                throw new ArgumentException("invalid argument length");
             }
             for (int i = 0; i < typesToAdd.Length; i++) {
                 if(typesToAdd[i]) {
@@ -33,22 +56,38 @@ namespace SadSapphicGames.CardEngineEditor {
             CloseWindow = true;
         }
     }
+    /// <summary>
+    /// The Popup Window through which the user interacts with the AddTypeObject
+    /// </summary>
     public class AddTypePopup : PopupWindowContent {
+        /// <summary>
+        /// The AddTypeObject that the popup window wraps
+        /// </summary>
         private AddTypeObject popupObject;
+        /// <summary>
+        /// Gets the list of TypeNames from the wrapped AddTypeObject
+        /// </summary>
         private List<string> TypeNames { get => popupObject.typeNames;}
+        /// <summary>
+        /// Boolean array for the types to add
+        /// </summary>
         bool[] typesToAdd;
+        /// <summary>
+        /// The CardSO to add the types too
+        /// </summary>
         private CardSO targetCardSO;
-
+        /// <summary>
+        /// Constructs a AddTypePopup and targets its wrapped AddTypeObject on a given CardSO
+        /// </summary>
         public AddTypePopup(CardSO target) : base() {
             targetCardSO = target;
             popupObject = new AddTypeObject(targetCardSO);
             typesToAdd = new bool[TypeNames.Count];
 
         }
-        public override void OnOpen() {
-            // Debug.Log($"{typeNames.Count} potential types to add");
-        }
-
+        /// <summary>
+        /// What will be displayed by the popup window
+        /// </summary>
         public override void OnGUI(Rect rect) {
             if(popupObject.CloseWindow) editorWindow.Close();
             GUILayout.Label("Select Types To Add", EditorStyles.boldLabel);
