e85c2573ced4ac8a017ff58bea5c75fbede45b69
cmwedin <cmwedin@gmail.com>
2022-09-15 16:29:55 -0400

    effect xml comments

--- Runtime/CardEffects/CompositeEffectSO.cs
+++ Runtime/CardEffects/CompositeEffectSO.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 namespace SadSapphicGames.CardEngine
 {
-    [CreateAssetMenu(fileName = "CompositeEffectSO", menuName = "CardEngineDevelopment/CompositeEffectSO", order = 0)]
+    //? Asset menu tagged using in development
+    // [CreateAssetMenu(fileName = "CompositeEffectSO", menuName = "CardEngineDevelopment/CompositeEffectSO", order = 0)]
+    /// <summary>
+    /// A scriptable object for encapsulating multiple child effects in a single object, used for representing Card effects
+    /// </summary>
     public class CompositeEffectSO : EffectSO {
+        /// <summary>
+        /// The children encapsulated in the CompositeEffectSO
+        /// </summary>
         [SerializeField] private List<EffectSO> subEffects = new List<EffectSO>();
+        /// <summary>
+        /// Public read only version of the subeffects
+        /// </summary>
         public ReadOnlyCollection<EffectSO> Subeffects { get => subEffects.AsReadOnly();}
+        /// <summary>
+        /// The controller of the effect
+        /// </summary>
         public override AbstractActor Controller {
             get => base.Controller; 
             set {
@@ -18,12 +31,22 @@ namespace SadSapphicGames.CardEngine
                 base.Controller = value;
             }
         }
+        /// <summary>
+        /// The number of child effects
+        /// </summary>
         public int ChildrenCount { get => subEffects.Count;}
+        /// <summary>
+        /// Preforms the effects of the CompositeEffect
+        /// </summary>
         public override void ResolveEffect() {
             foreach (var effect in subEffects) {
                 effect.ResolveEffect();                
             }
         }
+        /// <summary>
+        /// Adds a child effect to the composite
+        /// </summary>
+        /// <param name="desiredEffect">The effect to the composite</param>
         public void AddChildEffect(EffectSO desiredEffect) {
             EffectSO effectClone = (EffectSO)ScriptableObject.CreateInstance(desiredEffect.GetType());
             effectClone.name = $"{this.name}{desiredEffect.name}Subeffect";
@@ -31,6 +54,10 @@ namespace SadSapphicGames.CardEngine
             subEffects.Add(effectClone);
             AssetDatabase.SaveAssets();
         }
+        /// <summary>
+        /// Removes a child effect of the composite
+        /// </summary>
+        /// <param name="childEffect">THe effect to remove from the composite</param>
         public void RemoveChild(EffectSO childEffect) {
             if(subEffects.Remove(childEffect)) {
                 return;
--- Runtime/CardEffects/EffectInterfaces/ITargeted.cs
+++ Runtime/CardEffects/EffectInterfaces/ITargeted.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 
 namespace SadSapphicGames.CardEngine
 {
+    /// <summary>
+    /// An interface to indicate an effect is targeted
+    /// </summary>
     public interface ITargeted
     {
+        /// <summary>
+        /// The types that can be targeted
+        /// </summary>
         public Type TargetType { get; }
+        /// <summary>
+        /// The zone that the target can be selected from
+        /// </summary>
         public CardZone TargetZone { get; }
+        /// <summary>
+        /// The target of the effect
+        /// </summary>
         public GameObject Target { get; set; }
     }
 }
